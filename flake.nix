0069615d807495f6294fc029272244c6f0df896b
meir <31469876+meir@users.noreply.github.com>
2025-11-14 14:58:13 +0100

    fix issue in nix flake

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
         default = pkgs.callPackage ./izu.nix { inherit pkgs; };
         izuGenerate = pkgs.callPackage ./izu-generate.nix {
           inherit pkgs;
-          izu = packages.default;
+          izu = packages.${final.system}.default;
           formatter = "sxhkd";
           hotkeys = [ ];
         };
