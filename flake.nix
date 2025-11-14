5f63b8bb338e74671bfda6988c0ca53f3b8564a9
meir <31469876+meir@users.noreply.github.com>
2025-11-14 15:00:51 +0100

    Fix reference to system in izuGenerate package

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
         default = pkgs.callPackage ./izu.nix { inherit pkgs; };
         izuGenerate = pkgs.callPackage ./izu-generate.nix {
           inherit pkgs;
-          izu = packages.${system}.default;
+          izu = packages.${pkgs.system}.default;
           formatter = "sxhkd";
           hotkeys = [ ];
         };
