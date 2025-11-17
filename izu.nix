7ff514f0e0897c8ada8a8ad9789bb579b5782588
meir <31469876+meir@users.noreply.github.com>
2025-11-17 14:00:11 +0100

    update xkbcommon hash

--- izu.nix
+++ izu.nix
@@ -8,7 +8,7 @@ pkgs.buildGoApplication rec {
 
   file = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/xkbcommon/libxkbcommon/master/include/xkbcommon/xkbcommon-keysyms.h";
-    hash = "sha256-U5ibymrhoq+glsoB1gDIdgpMaoBp8ySccah7bUfojYc=";
+    hash = "sha256-/f17XP7ASLZR6LgTS/tnP6y2+mJDld7nMUz8W+FJli8=";
   };
 
   preBuild = ''
